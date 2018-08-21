4b05159fc946bc09817ce79d28a4570c2a0e435e
Kamil <kamil4525@gmail.com>
2018-08-21 15:18:22 +0200

    MapView setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:map_view/figure_joint_type.dart';
+import 'package:map_view/map_view.dart';
+import 'package:map_view/polygon.dart';
+import 'package:map_view/polyline.dart';
 import './pages/Home.dart' as homePage;
 import './pages/About.dart' as aboutPage;
 
-void main() => runApp(new MyApp());
+const API_KEY = "AIzaSyALQLUYBzyhgn7onshH989xBeiW6aUrm_I";
+
+void main() {
+  MapView.setApiKey(API_KEY);
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -25,18 +34,23 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new CupertinoTabScaffold(
-      tabBar: new CupertinoTabBar(
-          backgroundColor: new Color(0xFF454c54),
-          items: [
-            new BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.home, color: Colors.white,),
-              title: Text("Home", style: new TextStyle(color: Colors.white)),
-            ),
-            new BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.info, color: Colors.white,),
-              title: Text("O aplikacji", style: new TextStyle(color: Colors.white)),
-            ),
-          ]),
+      tabBar:
+          new CupertinoTabBar(backgroundColor: new Color(0xFF454c54), items: [
+        new BottomNavigationBarItem(
+          icon: Icon(
+            CupertinoIcons.home,
+            color: Colors.white,
+          ),
+          title: Text("Home", style: new TextStyle(color: Colors.white)),
+        ),
+        new BottomNavigationBarItem(
+          icon: Icon(
+            CupertinoIcons.info,
+            color: Colors.white,
+          ),
+          title: Text("O aplikacji", style: new TextStyle(color: Colors.white)),
+        ),
+      ]),
       tabBuilder: (context, index) {
         return CupertinoTabView(
           builder: (context) {
--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:map_view/map_view.dart';
+
+const API_KEY = "AIzaSyALQLUYBzyhgn7onshH989xBeiW6aUrm_I";
 
 class Home extends StatefulWidget {
   @override
@@ -7,6 +12,30 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
+  // load map
+  var mapView = new MapView();
+  CameraPosition cameraPosition;
+  Uri staticMapUri;
+  var compositeSubscription = new CompositeSubscription();
+  var staticMapProvider = new StaticMapProvider(API_KEY);
+  var userLocation;
+
+  List<Marker> _markers = <Marker>[
+    new Marker(
+      "1",
+      "Something fragile!",
+      45.52480841512737,
+      -122.66201455146073,
+      color: Colors.blue,
+      draggable: true, //Allows the user to move the marker.
+      markerIcon: new MarkerIcon(
+        "images/flower_vase.png",
+        width: 112.0,
+        height: 75.0,
+      ),
+    ),
+  ];
+  
   @override
   Widget build(BuildContext context) {
     // load images
@@ -21,31 +50,239 @@ class HomeState extends State<Home> {
       width: 480.0,
     );
 
+    // initial state
+    @override
+    initStatae() {
+      super.initState();
+      cameraPosition = new CameraPosition(Locations.portland, 2.0);
+      staticMapUri = staticMapProvider.getStaticUri(Locations.portland, 12,
+        width: 900, height: 400, mapType: StaticMapViewType.roadmap);
+    }
+
     return CupertinoPageScaffold(
-      backgroundColor: new Color(0xFFebe7de),
+        backgroundColor: new Color(0xFFebe7de),
         child: CustomScrollView(
-      slivers: <Widget>[
-        new CupertinoSliverNavigationBar(
-          largeTitle: Text('Gdzie jest moje auto?', style: new TextStyle(color: Colors.white),),
-          backgroundColor: new Color(0xFF454c54),
-        ),
-        new SliverFillRemaining(
-            child: new Container(
-          padding: EdgeInsets.all(5.0),
-          child: new Center(
-            child: new Column(children: <Widget>[
-              new Card(
-                  child: new Container(
-                child: parkCarImg,
-              )),
-              new Card(
-                  child: new Container(
-                child: findCarImg,
-              ))
-            ]),
-          ),
-        ))
-      ],
-    ));
+          slivers: <Widget>[
+            new CupertinoSliverNavigationBar(
+              largeTitle: Text(
+                'Gdzie jest moje auto?',
+                style: new TextStyle(color: Colors.white),
+              ),
+              backgroundColor: new Color(0xFF454c54),
+            ),
+            new SliverFillRemaining(
+                child: new Container(
+              padding: EdgeInsets.all(5.0),
+              child: new Center(
+                child: new Column(children: <Widget>[
+                  new GestureDetector(
+                    onTap: parkTheCar,
+                    child: new Card(
+                        child: new Container(
+                      child: parkCarImg,
+                    )),
+                  ),
+                  new GestureDetector(
+                      onTap: findTheCar,
+                      child: new Card(
+                          child: new Container(
+                        child: findCarImg,
+                      )))
+                ]),
+              ),
+            ))
+          ],
+        ));
+  }
+
+  void parkTheCar() {
+    print("park");
+    showLocation();
+    print("000000000000000000000000000000000");
+    print(userLocation);
+    Marker carMarker = new Marker(
+      "2",
+      "Something fragile!",
+      userLocation.latitude,
+      userLocation.longitude,
+      color: Colors.blue,
+      draggable: true, //Allows the user to move the marker.
+      markerIcon: new MarkerIcon(
+        "images/flower_vase.png",
+        width: 112.0,
+        height: 75.0,
+      ),
+    );
+    _markers.add(carMarker);
+  }
+
+  void findTheCar() {
+    print("find");
+    showMap();
+  }
+
+  // map methods
+  void showMap() {
+    mapView.show(
+        new MapOptions(
+            mapViewType: MapViewType.normal,
+            showUserLocation: true,
+            showMyLocationButton: true,
+            showCompassButton: true,
+            initialCameraPosition: new CameraPosition(
+                new Location(45.526607443935724, -122.66731660813093), 15.0),
+            hideToolbar: false,
+            title: "Recently Visited"),
+        toolbarActions: [new ToolbarAction("Close", 1)]);
+    StreamSubscription sub = mapView.onMapReady.listen((_) {
+      mapView.setMarkers(_markers);
+    });
+    compositeSubscription.add(sub);
+    sub = mapView.onLocationUpdated.listen((location) {
+      print("Location updated $location");
+    });
+    compositeSubscription.add(sub);
+    sub = mapView.onTouchAnnotation
+        .listen((annotation) => print("annotation ${annotation.id} tapped"));
+    compositeSubscription.add(sub);
+    sub = mapView.onMapTapped
+        .listen((location) => print("Touched location $location"));
+    compositeSubscription.add(sub);
+    sub = mapView.onCameraChanged.listen((cameraPosition) =>
+        this.setState(() => this.cameraPosition = cameraPosition));
+    compositeSubscription.add(sub);
+    sub = mapView.onAnnotationDragStart.listen((markerMap) {
+      var marker = markerMap.keys.first;
+      print("Annotation ${marker.id} dragging started");
+    });
+    sub = mapView.onAnnotationDragEnd.listen((markerMap) {
+      var marker = markerMap.keys.first;
+      print("Annotation ${marker.id} dragging ended");
+    });
+    sub = mapView.onAnnotationDrag.listen((markerMap) {
+      var marker = markerMap.keys.first;
+      var location = markerMap[marker];
+      print("Annotation ${marker.id} moved to ${location.latitude} , ${location
+          .longitude}");
+    });
+    compositeSubscription.add(sub);
+    sub = mapView.onToolbarAction.listen((id) {
+      print("Toolbar button id = $id");
+      if (id == 1) {
+        _handleDismiss();
+      }
+    });
+    compositeSubscription.add(sub);
+    sub = mapView.onInfoWindowTapped.listen((marker) {
+      print("Info Window Tapped for ${marker.title}");
+    });
+    compositeSubscription.add(sub);
+  }
+
+    void showLocation() {
+    mapView.show(
+        new MapOptions(
+            mapViewType: MapViewType.normal,
+            showUserLocation: true,
+            showMyLocationButton: true,
+            showCompassButton: true,
+            initialCameraPosition: new CameraPosition(
+                new Location(45.526607443935724, -122.66731660813093), 15.0),
+            hideToolbar: false,
+            title: "Zaparkowano samochód"),
+        toolbarActions: [new ToolbarAction("Close", 1)]);
+    StreamSubscription sub = mapView.onMapReady.listen((_) {
+      mapView.setMarkers(_markers);
+    });
+    compositeSubscription.add(sub);
+    sub = mapView.onLocationUpdated.listen((location) {
+      print("Location updated $location");
+      userLocation = location;
+    });
+    sub = mapView.onMapReady.listen((_) {
+      mapView.setMarkers(_markers);
+    });
+    compositeSubscription.add(sub);
+    sub = mapView.onTouchAnnotation
+        .listen((annotation) => print("annotation ${annotation.id} tapped"));
+    compositeSubscription.add(sub);
+    sub = mapView.onMapTapped
+        .listen((location) => print("Touched location $location"));
+    compositeSubscription.add(sub);
+    sub = mapView.onCameraChanged.listen((cameraPosition) =>
+        this.setState(() => this.cameraPosition = cameraPosition));
+    compositeSubscription.add(sub);
+    sub = mapView.onAnnotationDragStart.listen((markerMap) {
+      var marker = markerMap.keys.first;
+      print("Annotation ${marker.id} dragging started");
+    });
+    sub = mapView.onAnnotationDragEnd.listen((markerMap) {
+      var marker = markerMap.keys.first;
+      print("Annotation ${marker.id} dragging ended");
+    });
+    sub = mapView.onAnnotationDrag.listen((markerMap) {
+      var marker = markerMap.keys.first;
+      var location = markerMap[marker];
+      print("Annotation ${marker.id} moved to ${location.latitude} , ${location
+          .longitude}");
+    });
+    compositeSubscription.add(sub);
+    sub = mapView.onToolbarAction.listen((id) {
+      print("Toolbar button id = $id");
+      if (id == 1) {
+        _handleDismiss();
+      }
+    });
+    compositeSubscription.add(sub);
+    sub = mapView.onInfoWindowTapped.listen((marker) {
+      print("Info Window Tapped for ${marker.title}");
+    });
+    compositeSubscription.add(sub);
+  }
+
+  void _handleDismiss() async {
+    double zoomLevel = await mapView.zoomLevel;
+    Location centerLocation = await mapView.centerLocation;
+    List<Marker> visibleAnnotations = await mapView.visibleAnnotations;
+    print("Zoom Level: $zoomLevel");
+    print("Center: $centerLocation");
+    print("Visible Annotation Count: ${visibleAnnotations.length}");
+    var uri = await staticMapProvider.getImageUriFromMap(mapView,
+        width: 900, height: 400);
+    setState(() => staticMapUri = uri);
+    mapView.dismiss();
+    compositeSubscription.cancel();
+  }
+
+}
+
+class CompositeSubscription {
+  Set<StreamSubscription> _subscriptions = new Set();
+
+  void cancel() {
+    for (var n in this._subscriptions) {
+      n.cancel();
+    }
+    this._subscriptions = new Set();
+  }
+
+  void add(StreamSubscription subscription) {
+    this._subscriptions.add(subscription);
+  }
+
+  void addAll(Iterable<StreamSubscription> subs) {
+    _subscriptions.addAll(subs);
+  }
+
+  bool remove(StreamSubscription subscription) {
+    return this._subscriptions.remove(subscription);
+  }
+
+  bool contains(StreamSubscription subscription) {
+    return this._subscriptions.contains(subscription);
+  }
+
+  List<StreamSubscription> toList() {
+    return this._subscriptions.toList();
   }
 }
